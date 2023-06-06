dac2ef3318f8a294a449658e9d25f998259bb0df
FETYagency <fetyagency@gmail.com>
2023-06-06 12:32:54 +0100

    CHANGE TO THE SWITCH CASE

--- script/index.js
+++ script/index.js
@@ -47,64 +47,80 @@ theme_choice.forEach(e=>e.addEventListener("click", matchThemes))
 
 // the keypad constants
 const keys =  document.querySelectorAll(".key")
-const arith = document.querySelectorAll(".arithmetic")
-keys.forEach(e=>e.textContent=e.value)
-arith.forEach(e=>e.textContent=e.value)
-
-const arithmetic = document.querySelectorAll(".arithmetic")
 const result = document.querySelector(".result")
 const del = document.getElementById("delete");
 const reset = document.getElementById("reset");
 const equal = document.getElementById("equal");
 
+keys.forEach(e=>{
+    e.textContent=e.value;
+    
+    e.addEventListener("click", ()=>{
+        let arithRegex = /(?<=\d)[\+|\-|\/|\*|\.]{2}/g;
+        switch (e.value) {
+            case "*":
+                result.textContent+= e.value;
+                let one_multi = arithRegex.test(result.textContent)?result.textContent.split("").slice(0,-1).join(""):result.textContent;
+                result.textContent = one_multi;
+                break
+            ;
+            case "/":
+                result.textContent+= e.value;
+                let one_div =arithRegex.test(result.textContent)?result.textContent.split("").slice(0,-1).join(""):result.textContent;
+                result.textContent = one_div;
+                break
+            ;
+            case "+":
+                result.textContent+= e.value;
+                let one_add =arithRegex.test(result.textContent)?result.textContent.split("").slice(0,-1).join(""):result.textContent;
+                result.textContent = one_add;
+                break
+            ;
+            case "-":
+                result.textContent+= e.value;
+                let one_minus =arithRegex.test(result.textContent)?result.textContent.split("").slice(0,-1).join(""):result.textContent;
+                result.textContent = one_minus;
+                break;
+            ;
+            case ".":
+                result.textContent+= e.value;
+                let one_dot =arithRegex.test(result.textContent)?result.textContent.split("").slice(0,-1).join(""):result.textContent;
+                result.textContent = one_dot;
+                break;
+            ;
+            default:
+                if(result.textContent==="0"){
+                    result.textContent= ""
+                    result.textContent+= e.value;
+                }else{
+                    result.textContent+= e.value;
+                }
+            ;
+        }
+    })
+});
 
-function takeValue(e){
-    let regex = /[A-Za-z]/g
-    if(result.textContent==="0"||regex.test(result.textContent)){
-        result.textContent = "" +e.target.value
-    }else{
-        result.textContent +=e.target.value
-    }
-}
-
-
-function oneArithmetic(e){
-    let arithRegex = /(?<=\d)[\+|\-|\/|\*]{2}/g;
-    result.textContent+= e.target.value
-    if(arithRegex.test(result.textContent)){
-        let input =result.textContent.split("")
-        input.splice(input.length-1,1)
-        result.textContent = input.join("")
-    }
-}
-
-
-function resetResult(){
-    result.textContent = "0";
-}
-function deleteValue(){
-    let input =result.textContent.split("")
-    input.splice(input.length-1,1)
-    result.textContent = input.join("")
+reset.addEventListener("click",()=>{
+    result.textContent= "0";
+})
+del.addEventListener("click",()=>{
+    result.textContent=result.textContent.split("").slice(0,-1).join("")
     if(result.textContent.length<1){
-        result.textContent = 0
+        result.textContent= "0";
     }
-}
-
-function sum(){
-    try {
-        let value = eval(result.textContent)
-        result.textContent= value
-    } catch (error) {
-        result.textContent= "what was that??"
+})
+equal.addEventListener("click", ()=>{
+    try{
+        result.textContent= eval(result.textContent)
+        if(isNaN(result.textContent)){
+            throw(new Error("can't happen"))
+        }
+    }catch(err){
+        result.textContent=  err
         setTimeout(()=>{
-            result.textContent= 0;
-        },900)
+            result.textContent=  "0"
+        }, 900)
     }
-}
+})
 
-keys.forEach(e=>e.addEventListener("click", takeValue))
-arith.forEach(e=>e.addEventListener('click', oneArithmetic))
-del.addEventListener("click", deleteValue)
-reset.addEventListener("click", resetResult)
-equal.addEventListener('click',sum)
+console.log(0/0)
